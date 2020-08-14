5e3ae527ae9ff0a0a260415c4ab6bee04fbe6894
redryerye <yymmt3.waste@gmail.com>
2020-08-14 15:33:51 +0900

    initial commit

---
+++ env/dev/clj/portfolio/env.clj
@@ -0,0 +1,15 @@
+(ns portfolio.env
+  (:require
+    [selmer.parser :as parser]
+    [clojure.tools.logging :as log]
+    [portfolio.dev-middleware :refer [wrap-dev]]))
+
+(def defaults
+  {:init
+   (fn []
+     (parser/cache-off!)
+     (log/info "\n-=[portfolio started successfully using the development profile]=-"))
+   :stop
+   (fn []
+     (log/info "\n-=[portfolio has shut down successfully]=-"))
+   :middleware wrap-dev})
